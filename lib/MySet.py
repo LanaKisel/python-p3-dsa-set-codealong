df634dff60509aa570908ee710921a50a1d81b15
Lana Kisel <svetlana.kisel.ca@gmail.com>
2024-02-07 13:15:59 -0800

    myset

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,34 @@
 class MySet:
+    def __init__(self, enumerable = []):
+        self.dictionary = {}
+        for value in enumerable:
+            self.dictionary[value]=True
+        
+    def has(self, value):
+        return value in self.dictionary
+ 
+    def add(self, value):
+        self.dictionary[value] = True
+        return self
 
-    pass
+    def delete(self, value):
+        self.dictionary.pop(value, None)
+        return self
+
+    def size(self):
+        return len(self.dictionary)
+
+    def clear(self):
+        self.dictionary.clear()
+
+    def __str__(self):
+        set_list =[]
+        for key, value in self.dictionary.items():
+            set_list.append(str(key))
+        return f'MySet: {{{",".join(set_list)}}}'    
+
+
+set = MySet([1,2,3,3])
+print(set)
+set.clear()
+print(set)
